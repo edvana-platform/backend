3616424f60edecf427f611e2be35c38d611caa2f
hyguette-murinda <hyguettemurindaimfura@gmail.com>
2025-06-23 12:09:14 +0200

    add: swagger and render configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+/* eslint-disable */
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module'; 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Edvana API')
+    .setDescription('The API docs for Edvana learning platform')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
